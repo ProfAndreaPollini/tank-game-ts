43df002df7e0fd52b7d8eb80ad2ae5dae0b92251
Andrea Pollini <prof.andrea.pollini@gmail.com>
2022-06-18 11:09:28 +0200

    live20220618

---
+++ src/Tank.ts
@@ -0,0 +1,82 @@
+
+import * as PIXI from 'pixi.js'
+
+import Matter from "matter-js";
+// Matter Modules
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Body = Matter.Body;
+const Bodies = Matter.Bodies;
+const Mouse = Matter.Mouse;
+const MouseConstraint = Matter.MouseConstraint;
+const Composite = Matter.Composite;
+
+class Tank {
+  public sprite: PIXI.Sprite;
+  startText: PIXI.Text;
+  body: Matter.Composite;
+  mainBody: Matter.Body;
+
+  constructor(private engine: Matter.Engine, private app: PIXI.Application, public name: string, public image: string) {
+    this.sprite = PIXI.Sprite.from(image);
+    this.sprite.scale.set(0.5);
+    this.sprite.position.set(100, 100);
+
+    this.startText = new PIXI.Text(name, {
+      fontFamily: 'Arial',
+      fontSize: 12,
+      fill: 0xff1010,
+      align: 'center'
+    });
+
+    this.startText.x = this.sprite.position.x;
+    this.startText.y = this.sprite.position.y + 20 + this.startText.height;
+
+
+    app.stage.addChild(this.sprite);
+    app.stage.addChild(this.startText);
+
+     this.mainBody = Bodies.rectangle(
+      this.sprite.position.x,
+      this.sprite.position.y,
+      this.sprite.width,
+      this.sprite.height,
+      {
+        restitution: 0,
+        mass:1
+      }
+    );
+
+    this.body = Composite.create({ label: "tank" });
+    Composite.add(this.body, this.mainBody);
+
+    World.add(engine.world, this.body);
+  }
+
+
+  public setPosition(_x: number, _y: number) {
+    this.sprite.position.set(_x, _y);
+    this.mainBody.position.x = _x;
+    this.mainBody.position.y = _y;
+  }
+
+  public setPositionX(_x: number) {
+    this.sprite.position.x = _x;
+    this.body.position.x = _x;
+  }
+
+  public setTextPositionX(_x: number) {
+    this.startText.x = _x;
+  }
+
+  public setVelocity(_x: number, _y: number) {
+    Body.setVelocity(this.body, { x: _x, y: _y });
+  }
+
+  public setVelocityX(_x: number) {
+    Body.setVelocity(this.body, { x: _x, y: this.body.velocity.y });
+  }
+
+}
+
+export default Tank
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,64 @@
-import './style.css'
+import * as PIXI from 'pixi.js'
+import { Howl, Howler } from 'howler';
+import Matter from "matter-js";
+// Matter Modules
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Body = Matter.Body;
+const Bodies = Matter.Bodies;
+const Mouse = Matter.Mouse;
+const MouseConstraint = Matter.MouseConstraint;
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+import Tank from './Tank';
+//import './style.css'
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+// The application will create a renderer using WebGL, if possible,
+// with a fallback to a canvas render. It will also setup the ticker
+// and the root stage PIXI.Container.
+const app = new PIXI.Application({ width: 640, height: 360 });
+
+const engine = Engine.create();
+
+// The application will create a canvas element for you that you
+// can then insert into the DOM.
+document.body.appendChild(app.view);
+
+const wallBottom = Bodies.rectangle(
+  0,
+  app.view.height - 30,
+  app.view.width,
+  200,
+  {
+    isStatic: true,
+    restitution: 0,
+    mass: 10000
+  }
+);
+
+World.addBody(engine.world, wallBottom)
+
+
+const t = new Tank(engine, app, "pinco pallo", "../assets/PNG/Default size/tanks_tankDesert1.png");
+t.sprite.tint = Math.random() * 0xFFFFFF << 0
+
+// Add a ticker callback to move the sprite back and forth
+let elapsed = 0.0;
+// app.ticker.add((delta) => {
+
+//   t.body.position = t.sprite.position;
+//   t.body.angle = t.sprite.rotation;
+// });
+
+app.ticker.add((delta) => {
+  elapsed += delta;  
+
+
+  t.setVelocityX(Math.cos(elapsed / 150.0) / delta);
+  
+  console.log(t.body.position)
+
+  t.setTextPositionX(t.body.position.x)
+  t.sprite.position = t.body.position;
+})
+
+Matter.Runner.run(engine);
